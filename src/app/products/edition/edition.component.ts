66fd5813801adf06f18c133443c1a60d467895cf
leomsa <leo.akimim@gmail.com>
2024-09-02 16:52:03 -0300

    Creating metodh to save products with formBuilder.

--- src/app/products/edition/edition.component.ts
+++ src/app/products/edition/edition.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ProductsService} from "src/app/products/service/products.service";
 @Component({
   selector: 'app-edition',
   templateUrl: './edition.component.html',
   styleUrls: ['./edition.component.scss']
 })
-export class EditionComponent {
+export class EditionComponent implements OnInit {
 
+  productForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private productService: ProductsService,
+    private router: Router
+  ) {
+    this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      supplier: ['', Validators.required],
+      price: [0, [Validators.required, Validators.min(0)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.productForm.valid) {
+      const product = this.productForm.value;
+      this.productService.createProduct(product).subscribe({
+        next: () => {
+          this.router.navigate(['/products']);
+        },
+        error: (err) => {
+          console.error('Erro ao salvar produto', err);
+        }
+      });
+    }
+  }
+
+  ngOnInit(): void {
+  }
 }
