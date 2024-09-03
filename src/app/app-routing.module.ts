5e9d99688ca62fe16bfe6fe04c4b9917eee44075
Leonardo Machado dos Santos <leonardo.machadodossantos@agcocorp.com>
2024-09-03 14:58:50 -0300

    Created rout to login first page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import {RouterModule, Routes} from '@angular/router';
 
 
 const routes: Routes = [
-  // {path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule)},
-  // {path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule)},
-  // {path: '', redirectTo: '/login', pathMatch: 'full'},
-  // {path: '**', redirectTo: '/login'}
+  { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
 @NgModule({
